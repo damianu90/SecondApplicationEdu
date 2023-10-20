906650969de1e935ffb1e658893f373208561c4b
damianu90 <damian.uzieblo@gmail.com>
2023-10-20 20:19:22 +0200

    Dodanie implementacji głównej klasy ViewModel

---
+++ app/src/main/java/com/damianu/secondapplicationedu/MainViewModel.kt
@@ -0,0 +1,36 @@
+package com.damianu.secondapplicationedu
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.damianu.secondapplicationedu.data.TransactionsRepository
+import com.damianu.secondapplicationedu.data.model.Transaction
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainViewModel(app: Application): AndroidViewModel(app) {
+    private val repo = TransactionsRepository(app.applicationContext)
+
+    fun insertTransaction(transaction: Transaction) =
+        CoroutineScope(Dispatchers.IO).launch {
+            repo.insertTransaction(transaction)
+        }
+
+    fun updateTransaction(transaction: Transaction) =
+        CoroutineScope(Dispatchers.IO).launch {
+            repo.updateTransaction(transaction)
+        }
+
+    fun deleteTransaction(transactions: List<Transaction>) =
+        CoroutineScope(Dispatchers.IO).launch {
+            repo.deleteTransactions(transactions)
+        }
+
+    fun getAllTransactions() = repo.getAllTransactions().asLiveData(viewModelScope.coroutineContext)
+    fun getAllIncomes() = repo.getAllIncomes().asLiveData(viewModelScope.coroutineContext)
+    fun getAllOutcomes() = repo.getAllOutcomes().asLiveData(viewModelScope.coroutineContext)
+    fun getSumOfIncomeByCategory() = repo.getSumOfIncomeByCategory().asLiveData(viewModelScope.coroutineContext)
+    fun getSumOfOutcomeByCategory() = repo.getSumOfOutcomeByCategory().asLiveData(viewModelScope.coroutineContext)
+}
--- app/src/main/java/com/damianu/secondapplicationedu/data/TransactionsRepository.kt
+++ app/src/main/java/com/damianu/secondapplicationedu/data/TransactionsRepository.kt
@@ -1,4 +1,31 @@
 package com.damianu.secondapplicationedu.data
 
-class TransactionsRepository {
+import android.content.Context
+import com.damianu.secondapplicationedu.data.model.Transaction
+import com.damianu.secondapplicationedu.data.room.DatabaseInstance
+
+class TransactionsRepository(context: Context) {
+    private val transactionsDao = DatabaseInstance.getInstance(context).transactionsDao()
+
+    suspend fun insertTransaction(transaction: Transaction) {
+        transactionsDao.insertTransaction(transaction)
+    }
+
+    suspend fun updateTransaction(transaction: Transaction) {
+        transactionsDao.updateTransaction(transaction)
+    }
+
+    suspend fun deleteTransactions(transactions: List<Transaction>) {
+        transactionsDao.deleteTransactions(transactions)
+    }
+
+    fun getAllTransactions() = transactionsDao.getAllTransactions()
+
+    fun getAllIncomes() = transactionsDao.getAllIncomes()
+
+    fun getAllOutcomes() = transactionsDao.getAllOutcomes()
+
+    fun getSumOfIncomeByCategory() = transactionsDao.getSumOfIncomesGroupByCategory()
+
+    fun getSumOfOutcomeByCategory() = transactionsDao.getSumOfOutcomesGroupByCategory()
 }
